f7ae8307dc3c9844da0dc199ec6692fc6a61a540
Dipesh, Korgaonkar <dipeshkorgaonkar2017@gmail.com>
2025-11-23 19:20:38 +0530

    20-sept-25

---
+++ src/ex_04_Operator/Lab042_Interview_Concat_Plus.java
@@ -0,0 +1,23 @@
+package ex_04_Operator;
+
+public class Lab042_Interview_Concat_Plus {
+    public void main(String[] args) {
+        int a =10;
+        int b =20;
+
+        System.out.println(a+b);
+
+        //+ ->String
+        String first_name="Dipesh";
+        String last_name="Korgaonkar";
+        System.out.println(first_name+last_name);
+//        System.out.println(first_name/last_name);
+//        System.out.println(first_name/last_name);
+//        System.out.println(first_name/last_name);
+
+        //  + -> bhave differently with the data type.
+        // + -> operator overloading
+
+    }
+
+}
